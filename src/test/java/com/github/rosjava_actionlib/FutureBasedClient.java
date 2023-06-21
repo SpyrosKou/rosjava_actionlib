6def9664645218480056d7bc9fe58327e4b25c5c
Spyros Koukas <21125056+SpyrosKou@users.noreply.github.com>
2023-06-21 20:58:04 +0300

    Cache connection state

--- src/test/java/com/github/rosjava_actionlib/FutureBasedClient.java
+++ src/test/java/com/github/rosjava_actionlib/FutureBasedClient.java
@@ -1,4 +1,5 @@
 /**
+ * Copyright 2015 Ekumen www.ekumenlabs.com
  * Copyright 2020 Spyros Koukas
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,6 +21,7 @@ import actionlib_msgs.GoalStatus;
 import actionlib_msgs.GoalStatusArray;
 import actionlib_tutorials.*;
 import com.google.common.base.Stopwatch;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
@@ -28,18 +30,18 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Demonstrates the {@link ActionFuture} usage
  *
  * @author Spyros Koukas
  */
-class FutureBasedClient extends AbstractNodeMain implements ActionClientListener<FibonacciActionFeedback, FibonacciActionResult> {
+final class FutureBasedClient extends AbstractNodeMain implements ActionClientListener<FibonacciActionFeedback, FibonacciActionResult> {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    private ActionClient actionClient = null;
-
-    private volatile boolean isStarted = false;
+    private ActionClient<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult> actionClient = null;
 
 
     @Override
@@ -47,30 +49,31 @@ class FutureBasedClient extends AbstractNodeMain implements ActionClientListener
         return GraphName.of("fibonacci_future_client");
     }
 
+    private final CountDownLatch connectionCountDownLatch = new CountDownLatch(1);
+    private final AtomicBoolean connectedToServerCache = new AtomicBoolean(false);
 
-    /**
-     * @param seconds the maximum time to wait before the client is started
-     * @return
-     */
     public final boolean waitForServerConnection(final long timeout, final TimeUnit timeUnit) {
-        final Stopwatch stopwatch = Stopwatch.createStarted();
-        while (!this.isStarted && stopwatch.elapsed(timeUnit) <= timeout) {
-            this.sleep(100);
-        }
-        if (this.isStarted) {
+        if (this.connectedToServerCache.get()) {
+            return true;
+        } else {
+            final Stopwatch stopwatch = Stopwatch.createStarted();
 
-            boolean serverStarted = false;
             LOGGER.trace("Waiting for action server to start...");
-            serverStarted = this.actionClient.waitForActionServerToStart(Math.max(0, timeout - stopwatch.elapsed(timeUnit)), timeUnit);
-            if (serverStarted) {
-                LOGGER.trace("Action server started.\n");
-                return true;
-            } else {
-                LOGGER.trace("No actionlib server found after waiting for " + stopwatch.elapsed(timeUnit) + " " + timeUnit.name());
+            try {
+                final boolean nodeStarted = this.connectionCountDownLatch.await(Math.max(0, timeout - stopwatch.elapsed(timeUnit)), timeUnit);
+                final boolean clientStarted = nodeStarted && this.actionClient.waitForActionServerToStart(Math.max(0, timeout - stopwatch.elapsed(timeUnit)), timeUnit);
+                final boolean serverConnection = clientStarted && this.actionClient.waitForServerConnection(Math.max(0, timeout - stopwatch.elapsed(timeUnit)), timeUnit);
+                if (LOGGER.isTraceEnabled()) {
+                    LOGGER.trace("Action client started:[" + clientStarted + "] serverConnected:[" + serverConnection + "] nodeStarted:[" + nodeStarted + "]");
+                }
+                if (serverConnection) {
+                    this.connectedToServerCache.compareAndSet(false, true);
+                }
+                return serverConnection;
+            } catch (final Exception exception) {
+                LOGGER.error(ExceptionUtils.getStackTrace(exception));
                 return false;
             }
-        } else {
-            return false;
         }
 
     }
@@ -79,9 +82,10 @@ class FutureBasedClient extends AbstractNodeMain implements ActionClientListener
      * @param order
      * @return a future of the fibonacci
      */
-    public final ActionFuture<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult> invoke(final int order) {
+    public final ActionFuture<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult> invoke(
+            final int order) {
         // Create Fibonacci goal message
-        final FibonacciActionGoal goalMessage = (FibonacciActionGoal) actionClient.newGoalMessage();
+        final FibonacciActionGoal goalMessage = actionClient.newGoalMessage();
         final FibonacciGoal fibonacciGoal = goalMessage.getGoal();
         // set Fibonacci parameter
         fibonacciGoal.setOrder(order);
@@ -92,20 +96,19 @@ class FutureBasedClient extends AbstractNodeMain implements ActionClientListener
 
 
     @Override
-    public void onStart(final ConnectedNode node) {
-        this.actionClient = new ActionClient<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult>(node, "/fibonacci", FibonacciActionGoal._TYPE, FibonacciActionFeedback._TYPE, FibonacciActionResult._TYPE);
-        this.isStarted = true;
+    public final void onStart(final ConnectedNode connectedNode) {
+        this.actionClient = new ActionClient<>(connectedNode, "/fibonacci", FibonacciActionGoal._TYPE, FibonacciActionFeedback._TYPE, FibonacciActionResult._TYPE);
         // Attach listener for the callbacks
         this.actionClient.addListener(this);
-
+        this.connectionCountDownLatch.countDown();
     }
 
     /**
      * @param message
      */
     @Override
-    public void resultReceived(final FibonacciActionResult message) {
-        FibonacciResult result = message.getResult();
+    public final void resultReceived(final FibonacciActionResult message) {
+        final FibonacciResult result = message.getResult();
         int[] sequence = result.getSequence();
         int i;
 
@@ -120,8 +123,8 @@ class FutureBasedClient extends AbstractNodeMain implements ActionClientListener
      * @param message
      */
     @Override
-    public void feedbackReceived(final FibonacciActionFeedback message) {
-        FibonacciFeedback result = message.getFeedback();
+    public final void feedbackReceived(final FibonacciActionFeedback message) {
+        final FibonacciFeedback result = message.getFeedback();
         int[] sequence = result.getSequence();
         int i;
 
@@ -135,14 +138,16 @@ class FutureBasedClient extends AbstractNodeMain implements ActionClientListener
      * @param status The status message received from the server.
      */
     @Override
-    public void statusReceived(final GoalStatusArray status) {
+    public final void statusReceived(final GoalStatusArray status) {
         if (LOGGER.isInfoEnabled()) {
             final List<GoalStatus> statusList = status.getStatusList();
             for (final GoalStatus goalStatus : statusList) {
                 LOGGER.info("GoalID: " + goalStatus.getGoalId().getId() + " GoalStatus: " + goalStatus.getStatus() + " - " + goalStatus.getText());
             }
+
         }
     }
+
 
     /**
      * @param msec
