e6c24f6da44f80cd65a5866ee6e40fb959288681
Spyros Koukas <21125056+SpyrosKou@users.noreply.github.com>
2024-03-15 21:25:21 +0200

    Fibonacci calculator can handle feedback

--- src/test/java/com/github/rosjava_actionlib/FibonacciCalculator.java
+++ src/test/java/com/github/rosjava_actionlib/FibonacciCalculator.java
@@ -1,10 +1,13 @@
 package com.github.rosjava_actionlib;
 
+import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.Runnables;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
@@ -13,11 +16,20 @@ import java.util.function.Supplier;
  * @author Spyros Koukas
  */
 final class FibonacciCalculator {
-    final int[] fibonacciSequence(int order) {
-        return this.fibonacciSequence(order, () -> Boolean.FALSE, Runnables::doNothing);
+    final int[] fibonacciSequence(final int order) {
+        return this.fibonacciSequence(order, () -> Boolean.FALSE, Runnables::doNothing,x->Runnables.doNothing(),Integer.MAX_VALUE);
     }
 
-    final int[] fibonacciSequence(int order, final Supplier<Boolean> shouldCancel, final Runnable onCancel) {
+    final int[] fibonacciSequence(final int order,final Consumer<List<Integer>> updater,final int updateInterval) {
+        return this.fibonacciSequence(order, () -> Boolean.FALSE, Runnables::doNothing,updater,updateInterval);
+    }
+    final int[] fibonacciSequence(final int order,final Supplier<Boolean> shouldCancel, final Runnable onCancel) {
+        return this.fibonacciSequence(order, shouldCancel, onCancel,x->Runnables.doNothing(),Integer.MAX_VALUE);
+    }
+
+    final int[] fibonacciSequence(int order, final Supplier<Boolean> shouldCancel, final Runnable onCancel, final Consumer<List<Integer>> updater, final int updateInterval) {
+        Objects.requireNonNull(updater);
+
         final ArrayList<Integer> fibonacciList = new ArrayList<>();
 
         fibonacciList.add(0);
@@ -32,6 +44,9 @@ final class FibonacciCalculator {
                 final Integer iMinus1 = fibonacciList.get(i - 1);
                 final Integer iMinus2 = fibonacciList.get(i - 2);
                 fibonacciList.add(iMinus1 + iMinus2);
+                if(i%updateInterval==0){
+                    updater.accept(new ArrayList<>(fibonacciList));
+                }
             }
         }
 
